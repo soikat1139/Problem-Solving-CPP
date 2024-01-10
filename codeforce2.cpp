1dfdca13eec5e20a786d931f162dffd9d02d25ae
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2024-01-10 22:09:47 +0600

    Create codeforce2.cpp

---
+++ codeforce2.cpp
@@ -0,0 +1,63 @@
+
+
+#include<iostream>
+
+#include<algorithm>
+
+
+#include<vector>
+#include<map>
+#include<string>
+
+
+
+
+
+int main(){
+
+
+    #ifndef ONLINE_JUDGE
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+
+    #endif
+
+    int n;
+
+    std::cin>>n;
+
+    int c7=0;
+    int c4=0;
+
+    for(int i=n/7;i>=0;i--){
+        if((n-7*i)%4==0){
+
+            c7=i;
+            c4=(n-7*i)/4;
+
+        }
+    }
+
+    if(c4==0 && c7==0){
+       std::cout<<"-1"; 
+    }
+
+    for(int i=0;i<c4;i++){
+        std::cout<<'4';
+    }
+    for(int i=0;i<c7;i++){
+        std::cout<<'7';
+    }
+
+    
+
+
+
+
+
+    return 0;
+}
+
+ 
